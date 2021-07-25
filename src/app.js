0778cb9d16cfa24aa2a0ee64576166e437bb2d5b
gabriellevilamar <gabrielle.vilamar@m4u.com.br>
2021-07-25 19:42:10 -0300

    removendo commits

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-
+const path = require('path');
 const app = express();
 
 // ==> Rotas da API:
-const index = require('./routes/index');
-// const productRoute = require('./routes/product.routes');
+const index = require('./routes');
+const productRoute = require('./routes/product.routes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.json({ type: 'application/vnd.api+json' }));
 app.use(cors());
 
-app.use(index);
-// app.use('/api/', productRoute);
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.use('/', index);
+app.use('/', productRoute);
 
 module.exports = app;
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,6 +1,8 @@
 const router = require('express-promise-router')();
+const { response } = require('express');
 const productController = require('../controllers/product.controller');
 // ==> Definindo as rotas do CRUD - 'Product':
 // ==> Rota responsável por criar um novo 'Product': (POST): localhost:3000/admin-loja-ximbole_bahiano/products
 router.post('/products', productController.createProduct);
+
 module.exports = router;
